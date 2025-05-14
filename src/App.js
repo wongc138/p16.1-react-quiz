310059f83a17804326d5299ec9147230de9bec9d
wongc138 <wongc138@uni.coventry.ac.uk>
2025-05-14 19:53:56 +0100

    16-08 btn dispatch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -26,6 +27,11 @@ function reducer(state, action) {
       return {
         ...state,
         status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
       };
 
     default:
@@ -53,7 +59,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to the Quiz</h2>
       <h3> {numQuestions} questions to test your React mastery!</h3>
-      <button className="btn btn-ui">Start Quiz</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Start Quiz
+      </button>
     </div>
   );
 }
