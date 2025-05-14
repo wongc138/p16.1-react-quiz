9fb4ee5c3fccc08fb7b552159288d698d132e664
wongc138 <wongc138@uni.coventry.ac.uk>
2025-05-14 18:59:40 +0100

    16-06 Api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  // The status of the quiz can be "loading", "ready", "active", or "finished"
+  status: "loading",
+  currentQuestionIndex: 0,
+  score: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="App">
       <Header />
